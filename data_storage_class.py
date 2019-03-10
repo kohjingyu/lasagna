fa58044ef7ae9a3664e36c6c7eee4f7fc2abc1bc
skool-handle <smurfination1@gmail.com>
2019-03-11 03:16:42 +0800

    added dataclass and toy dataclass in pickles.

---
+++ data_storage_class.py
@@ -0,0 +1,157 @@
+import copy
+class result_storage():
+    def __init__(self,is_test,batch_size,num_classes,num_batches):
+        self.is_test = is_test
+        self._TPlist={}
+        self._FPlist={}  #format =>   TPlist [epochnumber][pvalue]["TP or TN or FN or FP"]
+        self._TNlist={}
+        self._FNlist={}
+        self._train_losses={} # sums up all train losses. Key = epochnumber
+        self._val_losses = {} # sums up all validation losses Key = epochnumber
+        # of note is that it's called val losses here but it might not actually be so.
+        # it could be test loss
+        self._val_accuracies = {} # format => val_accuracies[epochnumber][threshold]
+        # same goes for this.
+        self._num_batches = num_batches
+        self._classwise_acc={} #format=> classwise_accuracy[epochnumber][classnumber]
+        self._batch_size = batch_size # for internal calculations
+        self._num_classes = num_classes # for internal calculations.
+    
+    
+    def accuracy_calculation_epoch(self,epoch):
+        # calculate accuracy. called before you obtain accuracies.
+        # ONLY CALL THIS ONCE. don't call before every obtain for the same epoch. you're just screwing it up now.
+        # Shaun will probably cry. he doesn't want to rewrite this again.
+        #did you know he rewrote this 3 times because he thought the structure was too messy
+        # anyway its still messy. what a failure.
+        for threshold_value in self._val_accuracies[epochs].keys():
+                self._val_accuracies[epochs][threshold_value]=self._val_accuracies[epochs][threshold_value]/(self._num_classes*self._batch_size*self._num_batches)
+
+    
+    def classwise_acc(self,epoch,classnumber):
+        """
+        returns a dictionary of the TP,FP,TN,FN
+        for this particular class, of this particular epoch.
+        Return value: dictionary[threshold] 
+        Dictionary is freshly created.
+        """
+        return_dict = {}
+        for threshold in self._classwise_acc[epoch].keys():
+            return_dict[threshold] = self._classwise_acc[epoch][threshold][classnumber]
+        return return_dict
+    
+    def store_train_loss(self,epoch,results):
+        """Where results refers to some loss function's output."""
+        if epoch not in self._train_losses.keys():
+            self._train_losses[epoch] = []
+        self._train_losses[epoch].append(results.item())
+    
+    def train_losses(self,epoch):
+        """
+        returns train loss for a particular epoch
+        return value: some number.
+        """ 
+        
+        return self._train_losses[epoch]
+    
+    def losses(self,epoch):
+        """
+        returns validation losses for a particular epoch
+        return value: some number.
+        """
+        return self._val_losses[epoch] # generalised name again
+    
+    def accuracies(self,epoch):
+        """
+        returns validation or test accuracies for a particular epoch.
+        does not have classwise segmentation.
+        return value: dictionary[threshold values]
+        """    
+        return self._val_accuracies[epoch] #generalised name again
+    
+    
+    def TP_FPplz(self,epoch):
+        """
+        Please note that the values are only with regards to test or validation.
+        Although.. you could use this class to store training data too, by pretending it's test or validation
+        data. You nasty.
+        Returns a dictionary, with the following keys of 
+        TP, FP, FN, FP in that dictionary, and then a nested dictionary of pvalues.
+        It represents the number of TP, FP , FN and FP of that class of that epoch.
+        return value: dict["FP" or "TP" or whatever] [pvalue threshold]
+        """
+        return copy.deepcopy({"TP": self._TPlist[epoch],"FP": self._FPlist[epoch],"FN":self._FNlist[epoch],"TN": self._TNlist[epoch]})
+        
+    def data_entry(self,answers,results, epochs,output):
+        """ requires the following:
+            # The correct answers (answers)
+            # the latest validation results (to store the loss)
+            # the current epoch number.
+            #IF YOU WRITE A DUPLICATE NUMBER IT WILL OVERWRITE A PREVIOUS ENTRY.
+            # output of the latest evaluation, no you don't need to detach it, please don't.
+        """
+        if epochs not in self._TPlist.keys():
+            self._TPlist[epochs]={}
+            self._FPlist[epochs]={}
+            self._TNlist[epochs]={}
+            self._FNlist[epochs]={}
+            #append epochs
+        if epochs not in self._classwise_acc.keys():
+            self._classwise_acc[epochs] = {}
+            
+        if epochs not in self._val_losses.keys():
+            self._val_losses[epochs] = results.item() #add an entry for this epoch
+        else:
+            self._val_losses[epochs] = results.item() + self._val_losses[epochs] # sum up this batch's loss.
+        if epochs not in self._val_accuracies.keys():
+            self._val_accuracies[epochs] ={} #add an entry for this epoch
+            
+        for threshold in range(0,105,5): # 0-100 percent certainty values.
+            predicted = np.where(output.detach().cpu().numpy()>threshold*0.01,1,0)
+            correct = (predicted == answers).sum() #pure accuracy. Get those in the threshold that are correct
+            batchTP = np.sum(np.logical_and(predicted == 1, answers== 1))
+            batchTN = np.sum(np.logical_and(predicted == 0, answers == 0)) #total TP,TN,FP,FN
+            batchFP = np.sum(np.logical_and(predicted == 1, answers == 0)) # does not consider classwise
+            batchFN = np.sum(np.logical_and(predicted == 0, answers== 1))
+            if threshold not in self._val_accuracies[epochs].keys():
+                self._val_accuracies[epochs][threshold] = correct #if not in, append
+            else:
+                self._val_accuracies[epochs][threshold] = correct + self._val_accuracies[epochs][threshold]
+                # else, sum it up.
+            if threshold in self._TPlist[epochs].keys():
+                # check if this threshold has been recorded for this epoch thus far.
+                self._TPlist[epochs][threshold] = self._TPlist[epochs][threshold] + batchTP
+                self._FPlist[epochs][threshold] = self._FPlist[epochs][threshold] + batchFP
+                self._TNlist[epochs][threshold] = self._TNlist[epochs][threshold] + batchTN 
+                # add it to the current count for this epoch.
+                self._FNlist[epochs][threshold] = self._FNlist[epochs][threshold] + batchFN 
+            else:
+                self._TPlist[epochs][threshold] = batchTP
+                self._FPlist[epochs][threshold] = batchFP #initiate the count for this epoch
+                self._TNlist[epochs][threshold] = batchTN
+                self._FNlist[epochs][threshold] = batchFN
+
+            for image_number in range(predicted.shape[0]):
+                if threshold not in self._classwise_acc[epochs].keys():
+                    self._classwise_acc[epochs][threshold]={}
+                    for class_number in range(self._num_classes):
+                        self._classwise_acc[epochs][threshold][class_number]={"TN":0, "TP":0,"FN":0,"FP":0}
+                for class_number in range(self._num_classes):
+                    #reminder classwise_acc format=> classwise_accuracy[epochnumber][threshold][classnumber]
+                    if predicted[image_number][class_number]==answers[image_number][class_number]: 
+                        # this was a TRUE for this class
+                        if answers[image_number][class_number]==0:
+                            # TRUE NEGATIVE
+                            self._classwise_acc[epochs][threshold][class_number]["TN"] = self._classwise_acc[epochs][threshold][class_number]["TN"] +1
+                        else:
+                            # TRUE POSITIVE
+                            self._classwise_acc[epochs][threshold][class_number]["TP"] = self._classwise_acc[epochs][threshold][class_number]["TP"] +1
+                    else:
+                        #this was a WRONG prediction for this class
+                        if answers[image_number][class_number]==0 and predicted[image_number][class_number]==1:
+                            # FALSE NEGATIVE
+                            self._classwise_acc[epochs][threshold][class_number]["FP"] = self._classwise_acc[epochs][threshold][class_number]["FP"] +1
+                        else:
+                            # FALSE POSITIVE
+                            self._classwise_acc[epochs][threshold][class_number]["FN"] = self._classwise_acc[epochs][threshold][class_number]["FN"] +1  
+
--- train.py
+++ train.py
@@ -2,38 +2,40 @@ import torch
 from torchvision import transforms
 from torch.utils.data.dataset import Dataset
 from PIL import Image
-from data import get_tensor_from_data
-
+import torchvision
+import pickle
 import numpy as np
-
+from data import get_tensor_from_data
 import time
-
+import pickle
+from data_storage_class import result_storage
 # TODO: Set this as command line args
 batch_size = 16
 workers = 8 # How many cores to use to load data
 dev_mode = True # Set this to False when training on Athena
 total_ingredients = 30167 # Found by loading vocab.bin
-num_epochs = 10
 
-if not dev_mode:
-    from data_loader import ImagerLoader
-
+#############################
+# TOGGLES HERE
+learning_rate = 0.01
+momentum_mod = 0.01
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# device = torch.device("cpu")
+Epochal_loss=0
+num_classes = 30167
+total_epochs=30
+#############################
 if dev_mode:
     # Load from .npy file and batch manually
-    data_arr = np.load("./data/smalldata.npy") # shape: (100, 2)
+    data_arr = np.load("./data/smalldata.npy") # shape: (112, 2)
     num_batches = int(np.ceil(data_arr.shape[0] / batch_size))
-
     img_split = np.array_split(data_arr[:,0], num_batches)
     label_split = np.array_split(data_arr[:,1], num_batches)
     train_loader = [(img_split[i], label_split[i]) for i in range(num_batches)]
-
-    # For testing, just use same dataset
-    val_loader = [(img_split[i], label_split[i]) for i in range(num_batches)]
-    test_loader = [(img_split[i], label_split[i]) for i in range(num_batches)]
-else:
-    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                         std=[0.229, 0.224, 0.225])
-
+    val_loader = train_loader # you want to overfit anyway for testing.
+    test_loader = val_loader    
+else:    
+    from data_loader import ImagerLoader
     train_loader = torch.utils.data.DataLoader(
             ImagerLoader("./data/images/",
                 transforms.Compose([
@@ -42,7 +44,8 @@ else:
                 transforms.RandomCrop(224), # random crop within the center crop 
                 transforms.RandomHorizontalFlip(),
                 transforms.ToTensor(),
-                normalize,
+                transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                         std=[0.229, 0.224, 0.225]),
             ]), data_path="./data/lmdbs/", partition='train', sem_reg=None),
             batch_size=batch_size, shuffle=True,
             num_workers=workers, pin_memory=True)
@@ -53,74 +56,112 @@ else:
                 transforms.Scale(256), # rescale the image keeping the original aspect ratio
                 transforms.CenterCrop(224), # we get only the center of that rescaled
                 transforms.ToTensor(),
-                normalize,
+                transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                         std=[0.229, 0.224, 0.225]),
             ]), data_path="./data/lmdbs/", partition='val', sem_reg=None),
             batch_size=batch_size, shuffle=True,
             num_workers=workers, pin_memory=True)
 
-    test_loader = torch.utils.data.DataLoader(
-            ImagerLoader("./data/images/",
-                transforms.Compose([
-                transforms.Scale(256), # rescale the image keeping the original aspect ratio
-                transforms.CenterCrop(224), # we get only the center of that rescaled
-                transforms.ToTensor(),
-                normalize,
-            ]), data_path="./data/lmdbs/", partition='train', sem_reg=None),
-            batch_size=batch_size, shuffle=True,
-            num_workers=workers, pin_memory=True)
+#####################################################################
+num_batches = len(train_loader) # now we can get your batches since dataset is chosen
+#initialise your model here
+target_model = torchvision.models.squeezenet1_0(num_classes=num_classes)
+optimiser = torch.optim.SGD(target_model.parameters(), lr=learning_rate, momentum=momentum_mod)  # TOGGLES HERE.
+target_model = target_model.to(device)
+storage = result_storage(False,batch_size,num_classes,num_batches)
+test_result = result_storage(True,batch_size,num_classes,num_batches)
+###########################################################
 
-for epoch in range(num_epochs):
-    num_batches = len(train_loader)
 
+for epochs in range(total_epochs):
     for i, (input, target) in enumerate(train_loader):
         start = time.time()
-
-        # FOR LMDB FILES:
-        # input has 5 components
-        # x = img
-        # y1 = recipe step data  / torch.Size([1, 20, 1024]) ()
-        # y2 = number of steps / torch.Size([1])
-        # z1 = ingredient id / torch.Size([1, 20])
-        # z2 = number of ingredients / torch.Size([1])
         img_tensor, labels = get_tensor_from_data(input, target, dev_mode=dev_mode)
-
         # BIG DATA IS PROCESSED
-        # Now we have img_tensor, a (16, 3, 224, 224) Tensor of images, and labels, a (16, 30167) Tensor of labels
+        # Now we have a (16, 3, 224, 224) Tensor of images, and a (16, 30167) Tensor of labels
         # We can do d e e p l e a r n i n g
-
-        # TODO: Feed data into model, calculate loss and do backpropagation
-
+        # what a god.
+        #################################################
+        #TRAINING BIT
+        target_model.train() # set train mode
+        optimiser.zero_grad()
+        img_tensor = img_tensor.to(device)
+        target = labels.to(device)
+        output = torch.sigmoid(target_model(img_tensor.float()))
+        result = torch.nn.functional.binary_cross_entropy(output,target)
+        storage.store_train_loss(result.item())
+        #################################################
+        result.backward()
+        optimiser.step()
         time_taken = time.time() - start
-        print(f"Epoch {epoch}, batch {i} / {num_batches}, Time taken: {time_taken}s")
+        print("Epoch {}, batch{}, Training Loss - {}".format(epochs,i,result.item()))
+        print(f"Batch {i} / {num_batches}, Time taken: {time_taken}s")
+        print("="*20)
+        print("Starting validation...")
+        with torch.no_grad():
+            target_model.eval()
+            num_val_batches = len(val_loader)
+            for i, (input, target) in enumerate(val_loader):
+                start = time.time()
+                #############################################################################################
+                img_tensor, labels = get_tensor_from_data(input, target, dev_mode=dev_mode)
+                img_tensor = img_tensor.to(device)
+                labels = labels.to(device)
+                # or whatever God Koh Jing Yu did. i'm not even sure how much i've defaced it.
+                # did you hear he's a god and has actual coding standards
+                output = torch.sigmoid(target_model(img_tensor.float())) #sigmoid values. since it's binary cross entropy.
+                results = torch.nn.functional.binary_cross_entropy(output,labels) #calculate results.
+                answers = labels.cpu().numpy() #obtain a numpy version of answers.
+                storage.data_entry(answers,results,epochs,output)
+                # again, store losses
+                time_taken = time.time() - start
+                print("Time Taken: {}   Batch loss: {}".format(time_taken,results.item()))
+                #############################################################################################                
+            # calculate accuracy
+            print("Validation Accuracy for this epoch")
+            storage.accuracy_calculation_epoch(epochs) #calculate accuracies.
+            latest = storage.accuracies(epochs)
+            for threshold_value in latest.keys():
+                print("Threshold of {} :  {}".format(threshold_value,latest[threshold_value]))
+            
+            ###############################
+            ###############################
+            ###############################
+            # TODO: Early stop if required. 
+            ###############################
+            ###############################
+            ###############################
+            
 
     print("="*20)
-    print("Starting validation...")
-    # TODO: Perform validation
+    print("Starting test...")
+    # TODO: Perform test
+    correct =0
+    target_model.eval()
     with torch.no_grad():
-        num_val_batches = len(val_loader)
-        for i, (input, target) in enumerate(val_loader):
+        num_test_batches = len(test_loader)
+        for i, (input, target) in enumerate(test_loader):
             start = time.time()
-
             img_tensor, labels = get_tensor_from_data(input, target, dev_mode=dev_mode)
-
+            img_tensor = img_tensor.to(device)
+            labels = labels.to(device)
+            ################################################
+            output = torch.sigmoid(target_model(img_tensor.float()))
+            results = torch.nn.functional.binary_cross_entropy(output,labels)
+            test_result.data_entry(answers,results,epochs,output)
             time_taken = time.time() - start
-            print(f"Validation batch {i} / {num_val_batches}, Time taken: {time_taken}s")
-        # TODO: Calculate validation accuracy or something
-        # Early stop if required
-
-print("="*20)
-print("Starting test...")
-# TODO: Perform test
-with torch.no_grad():
-    num_test_batches = len(test_loader)
-    for i, (input, target) in enumerate(test_loader):
-        start = time.time()
-
-        img_tensor, labels = get_tensor_from_data(input, target, dev_mode=dev_mode)
-
-        time_taken = time.time() - start
-        # print(f"Test batch {i} / {num_test_batches}, Time taken: {time_taken}s")
-    # TODO: Calculate test accuracy
-    print("Test accuracy: {}".format(1))
-
-print("Done")
+            print("Epoch {}, batch{}, Time Taken: {} , Test Loss - {}".format(epochs,i,time_taken,results.item()))
+            ################################################
+        ############################################################
+        print("Test Accuracy for this epoch")
+        test_result.accuracy_calculation_epoch(epochs) #calculate accuracies.
+        latest = test_result.accuracies(epochs)
+        for threshold_value in latest.keys():
+            print("Threshold of {} :  {}".format(threshold_value,latest[threshold_value]))
+        ############################################################
+        
+    pickle.dump(storage,open("val_loss_class.pkl","wb"))
+    pickle.dump(test_result,open("test_results.pkl","wb"))    
+    print("Done 1 epoch. Pickles dumped again.")
+    
+    
